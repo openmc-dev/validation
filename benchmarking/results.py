7979ae7a9fa03e3ff42c0e5a5e819d31fa90b114
Paul Romano <paul.k.romano@gmail.com>
2020-11-13 08:16:45 -0600

    Add get_icsbep_dataframe function

--- benchmarking/results.py
+++ benchmarking/results.py
@@ -1,7 +1,23 @@
+import csv
+from pathlib import Path
+
 import pandas as pd
 
 
-def short_name(name):
+def abbreviated_name(name):
+    """Return short name for ICSBEP benchmark cases
+
+    Parameters
+    ----------
+    name : str
+        ICSBEP benchmark name, e.g. "pu-met-fast-021/case-2"
+
+    Returns
+    -------
+    str
+        Abbreviated name, e.g. "pmf21-2"
+
+    """
     model, *case = name.split('/')
     volume, form, spectrum, number = model.split('-')
     abbreviation = volume[0] + form[0] + spectrum[0]
@@ -13,6 +29,20 @@ def short_name(name):
 
 
 def get_result_dataframe(filename):
+    """Read the data from a file produced by the benchmarking script.
+
+    Parameters
+    ----------
+    filename : str
+        Name of a results file produced by the benchmarking script.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe with 'keff' and 'stdev' columns. The benchmark name is used as
+        the index in the dataframe.
+
+    """
     return pd.read_csv(
         filename,
         header=None,
@@ -20,3 +50,26 @@ def get_result_dataframe(filename):
         usecols=[0, 1, 2],
         index_col="name",
     )
+
+
+def get_icsbep_dataframe():
+    """Read the benchmark model k-effective means and uncertainties.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe with 'keff' and 'stdev' columns. The benchmark name is used as
+        the index in the dataframe.
+
+    """
+    cwd = Path(__file__).parent
+    index = []
+    keff = []
+    stdev = []
+    with open(cwd / 'uncertainties.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, skipinitialspace=True)
+        for benchmark, case, mean, uncertainty in reader:
+            index.append(f'{benchmark}/{case}' if case else benchmark)
+            keff.append(float(mean))
+            stdev.append(float(uncertainty))
+    return pd.DataFrame({'keff': keff, 'stdev': stdev}, index=index)
